f958d90b909b76ce487025d08be622485d060ac7
muzaffar-angularminds <muzaffar@angularminds.in>
2024-09-12 17:03:04 +0530

    Added PreviousURL component

--- app/src/components/DashboardBody.tsx
+++ app/src/components/DashboardBody.tsx
@@ -1,13 +1,14 @@
 import { GenerateURL } from "./GenerateURL"
+import { PreviousUrl } from "./PreviousUrl"
 
 export const DashboardBody: React.FC = () => {
     return(
         <div>
-            <div className="mt-16">
+            <div className="mt-16 p-3">
                 <GenerateURL />
             </div>
-            <div>
-
+            <div className="mt-10 mx-5 p-3">
+                <PreviousUrl />
             </div>
         </div>
     )

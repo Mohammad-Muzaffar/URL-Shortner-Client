7bc091842b3cefbc4c2f2b70fbbafb8f4f7c1413
muzaffar-angularminds <muzaffar@angularminds.in>
2024-09-12 18:02:51 +0530

    Added footer to Dashboard

--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -2,10 +2,8 @@
 export const Footer: React.FC = () => {
     return (
         <>
-
-
-            <footer className="bg-[#112A46] rounded-lg shadow m-4 dark:bg-gray-800">
-                <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-center">
+            <footer className="bg-[#112A46] shadow w-full bottom-0 dark:bg-gray-800">
+                <div className="p-4 flex items-center justify-center">
                     <span className="text-md text-gray-100 sm:text-center dark:text-gray-100">© 2024 <a href="https://flowbite.com/" className="hover:underline text-rose-500">Short-url</a>. All Rights Reserved.
                     </span>
                 </div>
--- app/src/pages/Dashboard.tsx
+++ app/src/pages/Dashboard.tsx
@@ -1,5 +1,6 @@
 import { Appbar } from "../components/Appbar";
 import { DashboardBody } from "../components/DashboardBody";
+import { Footer } from "../components/Footer";
 
 // Add Auth Guard:
 
@@ -7,11 +8,14 @@ export const Dashboard: React.FC = () => {
 
     return (
         <div className="bg-[#FEF8EC]">
-            <div className="border-solid border-2 sticky bg-[#FEF8EC] top-0 w-screen bg-opacity-70">
+            <div className="border-solid border-2 sticky bg-[#FEF8EC] top-0 w-full bg-opacity-70">
                 <Appbar />
             </div>
             <div className="text-[#112A46]">
                 <DashboardBody />
+            </div>
+            <div>
+                <Footer />
             </div>
         </div>
     );
